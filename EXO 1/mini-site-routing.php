421ac8d72c3d8dec4821348b16c81f681ed6f84b
nathanportier <nportiern@gmail.com>
2020-07-13 12:11:24 +0200

    Update mini-site-routing.php

--- EXO 1/mini-site-routing.php
+++ EXO 1/mini-site-routing.php
@@ -1,20 +1,23 @@
-<!DOCTYPE html>
 
-<html>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-    <title>mini-site-rooting</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content = "width-device-width, initial-scale-1.0">
+<nav class="menu">
+<ul>
+<li><a href="mini-site-routing.php?page=1">accueil</a></li>
+<li><a href="mini-site-routing.php?page=2">page 1</a></li>
+<li><a href="mini-site-routing.php?page=3">page 2</a></li>
+<li><a href="mini-site-rooting.php?page=connexion" >Connexion</a>
+</ul>
+</nav>
+<title> Mini-site-routing </title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<nav>
-    <a href="mini-site-rooting.php?page=1" >Accueil</a>
-    <a href="mini-site-rooting.php?page=2" >Page 2</a>
-    <a href="mini-site-rooting.php?page=3" >Page 3</a>
-    <a href="mini-site-rooting.php?page=connexion" >Connexion</a>
-</nav>
 
 <?php
+
 session_start();
 if(!isset($_SESSION["id"])){
     if(isset($_COOKIE["id"])){
@@ -24,16 +27,32 @@ if(!isset($_SESSION["id"])){
         header("location: mini-site-rooting.php?page=connexion");
     }
 }
-    if ($_GET["page"] == 1)
-    echo "<p> Accueil ! </p>"; 
-    else if ($_GET["page"] == 2)
-    echo "<p> Page 2 ! </p>";
-    else if ($_GET["page"] == 3)
-    echo "<p> Page 3 ! <p>"; 
-    else if ($_GET["page"] == "connexion")
-    include "connexion.php";
-    if(isset($_SESSION["id"]))
-    echo"<p> Login:" . $_SESSION["id"]
+if($_GET["page"] == 1)
+{
+echo "<h1> Accueil </h1>";
+include('../J08/EXO 2/EXO 02/vitrine-accueil.php');
+}
+if($_GET["page"] == 2)
+{
+echo "<h1> Page 1  </h1>";
+include('../J08/EXO 2/EXO 02/vitrine-programme.php');
+}
+if($_GET["page"] == 3)
+{
+echo "<h1> Page 2  </h1>";
+include('../EXO 2/EXO 02/vitrine-contact.php');
+var_dump($_GET["page"]); 
+}
+if($_GET["page"]== Connexion)
+{
+echo "<h1> Connexion <h1>";
+include("connexion.php");
+}
 ?>
+
+
+
+
+
 </body>
 </html>
