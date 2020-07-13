00f8fcb7aded556e45e53fe9745c592b8d4b068c
nathanportier <nportiern@gmail.com>
2020-07-13 12:11:21 +0200

    Create connexion.php

---
+++ EXO 1/connexion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<p><form action ="securite.php" method ="post">
+<div>
+<label for ="Login"> Login :</label>
+<input type ="text" name ="Login">
+</div>
+<br>
+<div>
+<label for="Password"> Password  :</label> 
+<input type ="text" name ="Password">
+</div>
+<input type ="submit" value ="Envoyer"/>
+</form>
+</body>
+</html>
